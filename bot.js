afbcea6fc5568f5f163be0aab70246cc012794df
omertr <omerswagboy@gmail.com>
2018-05-08 14:11:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,28 +1,15 @@
-const botSettings = require("./botsettings.json");
-const Discord = require("discord.js");
-const fs = require("fs");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const prefix = botSettings.prefix;
-
-const bot = new Discord.Client({disableEveryone: true});
-bot.commands = new Discord.Collection();
-
-fs.readdir("./cmds/", (err, files) => {
-    if(err) console.error(err);
-
-    let jsfiles = files.filter(f => f.split(".").pop() === "js");
-    if(jsfiles.length <= 0) {
-        console.log("Oepsie, er zijn nog geen commands!");
-        return;
-    }
-
-    console.log(`Loading ${jsfiles.length} command(s)!`);
-
-    jsfiles.forEach((f, i) => {    
-        let props = require(`./cmds/${f}`);
-        console.log(`${i + 1}: ${f} loaded!`);
-        bot.commands.set(props.help.name, props);
-    });
+client.on('ready', () => {
+    console.log('I am ready!');
 });
-bot.login(botSettings.token); 
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
